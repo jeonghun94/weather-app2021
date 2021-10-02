b6c851b8cdd74588caef1e727542dce32fcdd6a2
jeonghun94 <66016555+jeonghun94@users.noreply.github.com>
2021-10-02 22:13:49 +0900

    날씨앱 2.0 시작

---
+++ App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Weather App 2.0</Text>
+      <StatusBar style="light" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color:"white"
+  }
+});
